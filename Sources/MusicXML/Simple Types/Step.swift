fc6f0efa8ff8b67bcf647e3acca1b09361a4ef6d
Treata11 <treata11@yahoo.com>
2023-09-05 16:48:42 +0330

    Add SyntacticSugar func and behavior for the Step enum

--- Sources/MusicXML/Simple Types/Step.swift
+++ Sources/MusicXML/Simple Types/Step.swift
@@ -7,7 +7,7 @@
 
 /// The step type represents a step of the diatonic scale, represented using the English letters A
 /// through G.
-public enum Step: String {
+public enum Step: String, CaseIterable {
     case a = "A"
     case b = "B"
     case c = "C"
@@ -19,3 +19,14 @@ public enum Step: String {
 
 extension Step: Equatable {}
 extension Step: Codable {}
+
+extension Step {    // SyntacticSugar
+    public func nextCase() -> Step {
+        let allCases = Step.allCases
+        guard let currentIndex = allCases.firstIndex(of: self) else {
+            return self
+        }
+        let nextIndex = (currentIndex + 1) % allCases.count
+        return allCases[nextIndex]
+    }
+}
