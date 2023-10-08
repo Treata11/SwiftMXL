8ff21a1b44be3d70fa179ddfef0885bf3d6aacd4
Treata11 <treata11@yahoo.com>
2023-10-08 19:17:34 +0330

    Add `TimewisePartTests`; > Decoding works fine as tested, encoding needs attention!

--- Sources/SwiftMXL/Complex Types/Timewise/Timewise.Part.swift
+++ Sources/SwiftMXL/Complex Types/Timewise/Timewise.Part.swift
@@ -8,14 +8,14 @@
 import XMLCoder
 
 extension Timewise {
-    // In either format, the part element has an id attribute that
-    // is an IDREF back to a score-part in the part-list. Measures
-    // have a required number attribute (going from partwise to
-    // timewise, measures are grouped via the number).
-    //
-    // <!ATTLIST part
-    //    id IDREF #REQUIRED
-    // >
+    /// In either format, the part element has an id attribute that
+    /// is an IDREF back to a score-part in the part-list. Measures
+    /// have a required number attribute (going from partwise to
+    /// timewise, measures are grouped via the number).
+    ///
+    /// <!ATTLIST part
+    ///    id IDREF #REQUIRED
+    /// >
     public struct Part {
         // MARK: - Instance Properties
 
@@ -51,5 +51,16 @@ extension Timewise.Part: Codable {
         // Decode music data elements
         let musicDataContainer = try decoder.singleValueContainer()
         self.musicData = try musicDataContainer.decode([MusicData].self)
+    }
+}
+
+extension Timewise.Part: DynamicNodeEncoding {
+    public static func nodeEncoding(for key: CodingKey) -> XMLEncoder.NodeEncoding {
+        switch key {
+        case CodingKeys.id:
+            return .attribute
+        default:
+            return .element
+        }
     }
 }
---
+++ Tests/SwiftMXLTests/Complex Types/TimewisePartTests.swift
@@ -0,0 +1,111 @@
+//
+//  TimewisePartTests.swift
+//
+//
+//  Created by Treata Norouzi on 10/6/23.
+//
+
+import SwiftMXL
+import XCTest
+import XMLCoder
+
+class TimewiseTests: XCTestCase {
+    var decoder: XMLDecoder {
+        let decoder = XMLDecoder()
+            decoder.trimValueWhitespaces = false
+            decoder.removeWhitespaceElements = true
+        return decoder
+    }
+    var encoder: XMLEncoder {
+        let encoder = XMLEncoder()
+            encoder.outputFormatting = [.prettyPrinted]
+        return encoder
+    }
+
+    // TODO: Handle the DynamicNodeEncoding of Timewise
+    func testMeasureRoundTrip() {
+        let xml = """
+        <measure number="1">
+           <part id="P1">
+              <attributes>
+                 <divisions>1</divisions>
+                 <key>
+                    <fifths>0</fifths>
+                    <mode>major</mode>
+                 </key>
+                 <time symbol="common">
+                    <beats>4</beats>
+                    <beat-type>4</beat-type>
+                 </time>
+                 <clef>
+                    <sign>G</sign>
+                    <line>2</line>
+                 </clef>
+              </attributes>
+              <note>
+                 <pitch>
+                    <step>G</step>
+                    <octave>2</octave>
+                 </pitch>
+                 <duration>1</duration>
+                 <voice>1</voice>
+                 <type>quarter</type>
+              </note>
+              <note>
+                 <pitch>
+                    <step>A</step>
+                    <octave>2</octave>
+                 </pitch>
+                 <duration>1</duration>
+                 <voice>1</voice>
+                 <type>quarter</type>
+              </note>
+           </part>
+        </measure>
+        """
+        let decoded = try! decoder.decode(Timewise.Measure.self, from: xml.data(using: .utf8)!)
+        let part = Timewise.Part(
+            id: "P1",
+            musicData: [
+                .attributes(
+                    Attributes(
+                        divisions: 1,
+                        keys: [Key(fifths: 0, mode: .major)],
+                        times: [Time([Time.Signature(beats: ["4"], beatType: ["4"])], symbol: .common)],
+                        clefs: [Clef(sign: .g, line: 2)]
+                    )
+                ),
+                .note(
+                    Note(
+                       pitch: Pitch(step: .g, octave: 2),
+                       duration: 1,
+                       voice: "1",
+                       type: .quarter
+                   )
+                ),
+                .note(
+                    Note(
+                       pitch: Pitch(step: .a, octave: 2),
+                       duration: 1,
+                       voice: "1",
+                       type: .quarter
+                   )
+                ),
+            ]
+        )
+        let expected = Timewise.Measure(
+            number: "1",
+            parts: [part]
+        )
+        
+        print("""
+        decoded: \n\(decoded)
+        --------------------
+        expected: \n\(expected)
+        --------------------
+        encoded: \n
+        ------------------------------------------------------------------------
+        """)
+        XCTAssertEqual(decoded, expected)
+    }
+}
